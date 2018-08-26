2e1610d9b918a2b9608d8e6c966755287505273a
CBingle-Williams <cbingle-williams@spartaglobal.com>
2018-08-26 18:09:44 +0100

    working logic, update html, and css

---
+++ JSS/app.js
@@ -0,0 +1,45 @@
+var mathString = '';
+var first = false;
+
+function screen(string) {
+    if (first = false) {
+        document.getElementById('display').innerHTML = ' ';
+        first = true;
+    }
+    if  (string == 'lb') {
+        string = '(';
+    } else if (string == 'rb') {
+        string = ')';
+    }
+    mathString += string;
+    document.getElementById('display').innerHTML = mathString;
+}
+
+function negative () {
+    mathString = -Math.abs(parseFloat(mathString));
+    document.getElementById('display').innerHTML = mathString;
+    return;
+}
+
+function percentage () {
+    mathString = parseFloat(mathString)/100;
+    document.getElementById('display').innerHTML = mathString;
+    return;
+}
+
+function remove() {
+    mathString = '';
+    document.getElementById('display').innerHTML = '0.00';
+    first = false;
+    return;
+}
+
+function calculate() {
+    try {
+        document.getElementById('display').innerHTML = eval(mathString);
+    }
+    catch {
+        document.getElementById('display').innerHTML = 'Math Error!';
+    }
+    return;
+}
